0742d369ae87e6056a2fef4ddabbb7743fbd439e
NecRaul <necraul2001@gmail.com>
2023-08-31 02:56:12 +0400

    Downloading gists only when status code is 200 OK
    Printing status code and content otherwise

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ headers = {
 
 response = requests.get(API_ENDPOINT, headers=headers)
 
-for gist in response.json():
-    gist_id = gist["id"]
-    if not os.path.exists(gist_id):
-        os.mkdir(gist_id)
-    files = gist["files"]
-    for filename in files:
-        gist_url = files[filename]["raw_url"]
-        response = requests.get(gist_url, headers=headers)
-        with open(f"{gist_id}/{filename}", "wb") as file:
-            file.write(response.content)
+if response.status_code == 200:
+    for gist in response.json():
+        gist_id = gist["id"]
+        if not os.path.exists(gist_id):
+            os.mkdir(gist_id)
+        files = gist["files"]
+        for filename in files:
+            gist_url = files[filename]["raw_url"]
+            response = requests.get(gist_url, headers=headers)
+            with open(f"{gist_id}/{filename}", "wb") as file:
+                file.write(response.content)
+else:
+    print(response.status_code, response.text)
