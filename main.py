01273fb98f3f068cbddb6ccbc750f2dd5c9c32d1
NecRaul <necraul@kuroneko.dev>
2023-08-31 03:04:54 +0400

    Extracting download part to a function

--- main.py
+++ main.py
@@ -6,14 +6,8 @@ user = "NecRaul"
 
 API_ENDPOINT = f"https://api.github.com/users/{user}/gists"
 
-headers = {
-    "Authorization": f"token {public_access_token}",
-}
-
-response = requests.get(API_ENDPOINT, headers=headers)
-
-if response.status_code == 200:
-    for gist in response.json():
+def download_gists(gists):
+    for gist in gists:
         gist_id = gist["id"]
         if not os.path.exists(gist_id):
             os.mkdir(gist_id)
@@ -23,5 +17,15 @@ if response.status_code == 200:
             response = requests.get(gist_url, headers=headers)
             with open(f"{gist_id}/{filename}", "wb") as file:
                 file.write(response.content)
+
+headers = {
+    "Authorization": f"token {public_access_token}",
+}
+
+response = requests.get(API_ENDPOINT, headers=headers)
+
+
+if response.status_code == 200:
+    download_gists(response.json())
 else:
     print(response.status_code, response.text)
