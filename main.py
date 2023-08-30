504aa17298298912c60fa1cc2c8b5e5c0260c3ab
NecRaul <necraul@kuroneko.dev>
2023-08-31 03:33:05 +0400

    Simplified the code a bit

--- main.py
+++ main.py
@@ -9,24 +9,22 @@ API_ENDPOINT = f"https://api.github.com/users/{user}/gists"
 
 GIT = True
 
-def download_with_request(gists):
-    for gist in gists:
-        gist_id = gist["id"]
-        if not os.path.exists(gist_id):
-            os.mkdir(gist_id)
-        files = gist["files"]
-        for filename in files:
-            gist_url = files[filename]["raw_url"]
-            response = requests.get(gist_url, headers=headers)
-            with open(f"{gist_id}/{filename}", "wb") as file:
-                file.write(response.content)
+def download_with_request(gist):
+    gist_id = gist["id"]
+    if not os.path.exists(gist_id):
+        os.mkdir(gist_id)
+    files = gist["files"]
+    for filename in files:
+        gist_url = files[filename]["raw_url"]
+        response = requests.get(gist_url, headers=headers)
+        with open(f"{gist_id}/{filename}", "wb") as file:
+            file.write(response.content)
                 
-def download_with_git(gists):
-    for gist in gists:
-        if not os.path.exists(gist["id"]):
-            subprocess.call(["git", "clone", gist["git_pull_url"]])
-        else:
-            subprocess.call(["git", "pull", gist["git_pull_url"]])
+def download_with_git(gist):
+    if not os.path.exists(gist["id"]):
+        subprocess.call(["git", "clone", gist["git_pull_url"]])
+    else:
+        subprocess.call(["git", "pull", gist["git_pull_url"]])
     
 headers = {
     "Authorization": f"token {public_access_token}",
@@ -35,6 +33,7 @@ headers = {
 response = requests.get(API_ENDPOINT, headers=headers)
 
 if response.status_code == 200:
-    download_with_request(response.json()) if not GIT else download_with_git(response.json())
+    for gist in response.json():
+        download_with_request(gist) if not GIT else download_with_git(gist)
 else:
     print(response.status_code, response.text)
