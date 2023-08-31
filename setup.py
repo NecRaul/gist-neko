ed7a5dd9db3e61fcc72ec567017641e7494e6e7c
NecRaul <necraul2001@gmail.com>
2023-08-31 06:25:47 +0400

    Download only public gists if token isn't provided

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
-VERSION = "1.1"
+VERSION = "1.2"
 DESCRIPTION = "CLI for downloading all gists from a specified user."
-LONG_DESCRIPTION = "Download specified user's all gists at once. You can also use environment variables if you don't want to pass through arguments everytime you want to download a gist."
+LONG_DESCRIPTION = "Download specified user's all gists at once. You can also use environment variables if you don't want to pass through arguments everytime you want to download a gist. If you don't provide a public access token it will only download public gists."
 AUTHOR = "NecRaul"
 
 setup(
