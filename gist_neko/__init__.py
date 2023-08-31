8714a544e10ed61c41aeb7974e6c18b78f00a72b
NecRaul <necraul2001@gmail.com>
2023-08-31 04:25:12 +0400

    Parser arguments new attributes

--- gist_neko/__init__.py
+++ gist_neko/__init__.py
@@ -3,11 +3,12 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="Download specified user's all gists at once.")
-    parser.add_argument("-u", "--username", type=str, help="Github username")
-    parser.add_argument("-t", "--token", type=str, help="Github public access token")
-    parser.add_argument("-g", "--git", type=bool, help="Whether to download with git or not. False by default since it's dependent on whether or not git is downloaded (and your ssh/gpg key).")
+    parser.add_argument("-u", "--username", type=str, metavar="Username", help="Github username")
+    parser.add_argument("-t", "--token", type=str, metavar="Token", help="Github public access token")
+    parser.add_argument("-g", "--git", type=bool, metavar="True/False", help="Whether to download with git or not. False by default since it's dependent on whether or not git is downloaded (and your ssh/gpg key).")
     
     args = parser.parse_args()
+    
     username = args.username
     public_access_token = args.token
     git_check = args.git
