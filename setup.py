8379448b478d03861aed26ea69a93b8c17d3b124
NecRaul <necraul2001@gmail.com>
2023-10-07 12:19:59 +0400

    Added project links to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = "1.7"
+VERSION = "1.8"
 DESCRIPTION = "CLI for downloading all gists from a specified user."
 with open("README.md", "r") as file:
     LONG_DESCRIPTION = file.read()
@@ -16,6 +16,11 @@ setup(
     packages=find_packages(),
     install_requires=["requests"],
     keywords=["python", "gists downloader", "downloader", "gists", "gist-neko", "kuroneko"],
+    link="https://github.com/NecRaul/gist-neko",
+    project_urls={
+            'Documentation': 'https://github.com/NecRaul/gist-neko#readme',
+            'Source': 'https://github.com/NecRaul/gist-neko',
+        },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
