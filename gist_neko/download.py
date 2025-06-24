5386c39b8728425acbf640523c12ab2506f65438
NecRaul <necraul@kuroneko.dev>
2025-06-24 08:54:37 +0400

    Improve code quality
    Instead of looping through all gists and passing each gist
    to a function based on check, pass all of gists to the
    appropriate function and loop over them inside the function.

--- gist_neko/download.py
+++ gist_neko/download.py
@@ -4,30 +4,31 @@ import subprocess
 import shutil
 
 
-def with_request(gist, headers):
-    folder = name_folder(gist)
-    if not os.path.exists(folder):
-        os.mkdir(folder)
-        print(f"Downloading the gist '{folder}'...")
-    else:
-        print(f"Updating the gist '{folder}'...")
-        shutil.rmtree(folder)
+def download_with_requests(gists, headers):
+    for gist in gists:
+        folder = name_folder(gist)
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+            print(f"Downloading the gist '{folder}'...")
+        else:
+            print(f"Updating the gist '{folder}'...")
+            shutil.rmtree(folder)
+        files = gist["files"]
+        for filename in files:
+            gist_url = files[filename]["raw_url"]
+            response = requests.get(gist_url, headers=headers)
+            with open(f"{folder}/{filename}", "wb") as file:
+                file.write(response.content)
 
-    files = gist["files"]
-    for filename in files:
-        gist_url = files[filename]["raw_url"]
-        response = requests.get(gist_url, headers=headers)
-        with open(f"{folder}/{filename}", "wb") as file:
-            file.write(response.content)
 
-
-def with_git(gist):
-    folder = name_folder(gist)
-    gist_pull_url = f"git@gist.github.com:{gist['id']}.git"
-    if not os.path.exists(folder):
-        subprocess.call(["git", "clone", "--recursive", gist_pull_url, folder])
-    else:
-        subprocess.call(["git", "-C", folder, "pull", "--recurse-submodules"])
+def download_with_git(gists):
+    for gist in gists:
+        folder = name_folder(gist)
+        gist_pull_url = f"git@gist.github.com:{gist['id']}.git"
+        if not os.path.exists(folder):
+            subprocess.call(["git", "clone", "--recursive", gist_pull_url, folder])
+        else:
+            subprocess.call(["git", "-C", folder, "pull", "--recurse-submodules"])
 
 
 def name_folder(gist):
@@ -63,5 +64,7 @@ def download_gists(username, token, git_check):
     headers = {"Authorization": f"token {token}"} if token else None
     gists = get_all_gists(username, headers)
 
-    for gist in gists:
-        with_request(gist, headers) if not git_check else with_git(gist)
+    if git_check:
+        download_with_git(gists)
+    else:
+        download_with_requests(gists)
